063823988ca9c48a03a34fc4ab5b437cc86cbb4e
dinesh19-30 <dineshhariraman2004@gmail.com>
2026-02-16 21:17:26 +0530

    ex

---
+++ q1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String b=sc.nextLine();
+        int a=sc.nextInt();
+        System.out.println("My name is " + b + " and my age is " + a);
+    }
+    
+}
---
+++ q2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int age=sc.nextInt();
+        String address=sc.nextLine();
+        System.out.println("My name is " + name + " and my age is " + age + " and my address is " + address);
+    }
+    
+}
