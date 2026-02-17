177a4cf538b41c4dbfef4b8201edde08a8879a49
dinesh19-30 <dineshhariraman2004@gmail.com>
2026-02-17 19:43:11 +0530

    condition

---
+++ d2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class d2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>35 && b>40)
+        {
+            System.out.println("You are pass");
+        }
+        else
+        {
+            System.out.println("you are fail");
+        }
+
+    }
+}
