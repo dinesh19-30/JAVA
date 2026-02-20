dc894222318471759186fe1d716c69db910a891e
dinesh19-30 <dineshhariraman2004@gmail.com>
2026-02-20 20:27:25 +0530

    condition

---
+++ d3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class d3 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        if(a>b)
+        {
+            System.out.println("a is greater");
+            if(a%2==0)
+            {
+                System.out.println("this is even number");
+            }
+            else
+            {
+                System.out.println("this is odd number");
+            }    
+        }
+        else
+        {
+            System.out.println("b is greater");
+        }
+    }
+
+    
+}
